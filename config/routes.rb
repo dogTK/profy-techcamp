808cefd29b1c403776b9d66450a8b0759f4259f0
dogTK <koreeda@lrm-corp.jp>
2021-01-24 10:11:00 +0000

    初めてのコミット

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "top#index"
+  resources :groups, only: [:show, :edit, :update]
+  resources :questions, only: [:create, :show]
+  resources :answers, only: [:new, :create]
+  resources :answers, only: [:new, :create, :edit, :update]
+
+end
